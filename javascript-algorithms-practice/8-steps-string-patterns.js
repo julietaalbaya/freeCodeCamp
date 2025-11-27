f29a927ef162f081145c5939d9c2f9ec885828b5
julietaalbaya <julietaalbaya@hotmail.com.ar>
2025-11-27 09:25:13 -0300

    Create 8-steps-string-patterns.js
    El ejercicio 8 pide que, dado un número entero n, se debe hacer una escalera de n niveles usando el caracter #

---
+++ javascript-algorithms-practice/8-steps-string-patterns.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the step
+// has spaces on the right hand side!
+// --- Examples
+// steps(2)
+//     '# '
+//     '##'
+// steps(3)
+//     '#  '
+//     '## '
+//     '###'
+
+function steps(n) {
+    for (let row = 1; row <= n; row++) {
+        let step = '';
+        for (let col = 1; col <= n; col++) {
+            if (col <= row) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+        console.log(step);        
+    }    
+}
+
+steps(6);
